c66bed823f9f76a7568d7f0e4baf66bbf5f84361
xystudio <173288240@qq.com>
2025-04-26 12:26:31 +0800

    0.3.2

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup, find_packages
-import sys
-from img_fit import __version__
-
-data_files = []
-if sys.platform == "win32":
-    data_files = [('scripts',["scripts/img.exe"])]
 
 setup(
     name = "imgfit",
-    version = __version__,
-    packages = find_packages(),
+    version = "0.3.2",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires = ["pillow >= 8", "matplotlib >= 3.4", "PyLaTeXenc >= 2"],
-    python_requires = ">=3",
+    python_requires = ">=3.7",
     author = "xystudio",
     author_email = "173288240@qq.com",
     description = "Change your images.",
     long_description = open("README.md",encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     license = "MIT",
     url = "https://github.com/xystudio889/imgfit",
-    data_files = data_files,
-    include_package_data = True
+    entry_points={
+        "console_scripts": [
+            "img = img_fit.__init__:main",
+        ]
+    }, 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords = "image, fit, latex, pillow",
 )
